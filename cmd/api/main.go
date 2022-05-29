61325bd75e438c44098d6496e96c7c1e8b708ea8
vijeyash <vijeyash@gmail.com>
2022-05-29 14:29:09 +0530

    change

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,46 +11,48 @@ import (
 
 const version = "1.0.0"
 
-type Config struct {
+type config struct {
 	port int
 	env  string
 }
+
 type AppStatus struct {
 	Status      string `json:"status"`
 	Environment string `json:"environment"`
 	Version     string `json:"version"`
 }
+
 type application struct {
-	config Config
+	config config
 	logger *log.Logger
 }
 
 func main() {
-	var cfg Config
-	logger :=
-		log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
 
-	flag.IntVar(&cfg.port, "port", 4000, "port the server listens on")
-	flag.StringVar(&cfg.env, "env", "dev", "application environment: dev|pro")
-	flag.Parse()
-
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
-		IdleTimeout:  time.Minute,
 	}
-	logger.Println("starting server on port ", cfg.port)
+
+	logger.Println("Starting server on port", cfg.port)
 
 	err := srv.ListenAndServe()
 	if err != nil {
-		logger.Println(err)
+		log.Println(err)
 	}
-
 }
